8fb57c07905bbb3c62838a26f298232fbe8ca7dd
AmauryVdt <amaury.vdt@gmail.com>
2021-03-12 10:36:12 +0100

    apply checkstyle on the file Application.java

--- src/main/java/com/epsi/mycal/Application.java
+++ src/main/java/com/epsi/mycal/Application.java
@@ -2,9 +2,9 @@ package com.epsi.mycal;
 
 public class Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-	}
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Hello World!");
+  }
 
 }
